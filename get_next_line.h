c270c9e9e13ca330f4ae69c7a4b06e9980974683
eepylaurie <eepylaurie@gmail.com>
2025-12-01 22:52:51 +0100

    Add header file for get_next_line with function declarations and buffer size definition

---
+++ get_next_line.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmatthes <lmatthes@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/01 22:39:28 by lmatthes          #+#    #+#             */
+/*   Updated: 2025/12/01 22:52:05 by lmatthes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+
+size_t	ft_strlen_gnl(const char *s);
+char	*ft_strchr_gnl(const char *s, int c);
+char	*ft_strjoin_gnl(char const *s1, char const *s2);
+
+#endif
