a5e0cac4d29254819f524cf1c3be038f1be50bef
eepylaurie <eepylaurie@gmail.com>
2025-12-03 00:24:24 +0100

    Fix: refactor ft_strjoin_gnl and ft_init_s1 for improved readability and structure

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: lmatthes <lmatthes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/01 22:36:30 by lmatthes          #+#    #+#             */
-/*   Updated: 2025/12/03 00:05:45 by lmatthes         ###   ########.fr       */
+/*   Updated: 2025/12/03 00:20:19 by lmatthes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,12 +39,8 @@ char	*ft_strchr_gnl(const char *s, int c)
 	return (NULL);
 }
 
-char	*ft_strjoin_gnl(char *s1, char *s2)
+static char	*ft_init_s1(char *s1)
 {
-	char	*new;
-	size_t	i;
-	size_t	j;
-
 	if (!s1)
 	{
 		s1 = malloc(1);
@@ -52,17 +48,30 @@ char	*ft_strjoin_gnl(char *s1, char *s2)
 			return (NULL);
 		s1[0] = '\0';
 	}
-	if (!s2)
+	return (s1);
+}
+
+char	*ft_strjoin_gnl(char *s1, char *s2)
+{
+	char	*new;
+	size_t	i;
+	size_t	j;
+
+	s1 = ft_init_s1(s1);
+	if (!s1)
 		return (NULL);
 	new = malloc(ft_strlen_gnl(s1) + ft_strlen_gnl(s2) + 1);
 	if (!new)
 		return (free(s1), NULL);
 	i = 0;
 	while (s1[i])
-		new[i] = s1[i], i++;
+	{
+		new[i] = s1[i];
+		i++;
+	}
 	j = 0;
 	while (s2[j])
-		new[i] = s2[j], i++, j++;
+		new[i++] = s2[j++];
 	new[i] = '\0';
 	free(s1);
 	return (new);
