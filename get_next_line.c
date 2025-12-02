0d0578c3148a27f6334e62e91f339ffa6170e5aa
eepylaurie <eepylaurie@gmail.com>
2025-12-02 23:36:58 +0100

    Implement get_next_line function to read from file descriptor and manage stash

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: lmatthes <lmatthes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/02 21:41:35 by lmatthes          #+#    #+#             */
-/*   Updated: 2025/12/02 23:29:26 by lmatthes         ###   ########.fr       */
+/*   Updated: 2025/12/02 23:35:32 by lmatthes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,4 +63,31 @@ static char	*ft_update_stash(char *stash)
 	new[j] = '\0';
 	free(stash);
 	return (new);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*stash;
+	char		*buffer;
+	char		*line;
+	int			bytes;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		return (NULL);
+	bytes = 1;
+	while (!ft_strchr_gnl(stash, '\n') && bytes > 0)
+	{
+		bytes = read(fd, buffer, BUFFER_SIZE);
+		if (bytes < 0)
+			return (free(buffer), NULL);
+		buffer[bytes] = '\0';
+		stash = ft_strjoin_gnl(stash, buffer);
+	}
+	free(buffer);
+	line = ft_extract_line(stash);
+	stash = ft_update_stash(stash);
+	return (line);
 }
