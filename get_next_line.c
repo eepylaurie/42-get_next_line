16f603046dc3ac2ab2ccacd1e1efe678ba400cbc
eepylaurie <eepylaurie@gmail.com>
2025-12-02 23:28:01 +0100

    Implement ft_update_stash function to free the old stash and manage leftover characters

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: lmatthes <lmatthes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/02 21:41:35 by lmatthes          #+#    #+#             */
-/*   Updated: 2025/12/02 23:11:10 by lmatthes         ###   ########.fr       */
+/*   Updated: 2025/12/02 23:24:58 by lmatthes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,4 +38,27 @@ static char	*ft_extract_line(char *stash)
 	}
 	line[i] = '\0';
 	return (line);
+}
+
+static char	*ft_update_stash(char *stash)
+{
+	char	*new;
+	int		i;
+	int		j;
+
+	i = 0;
+	while (stash[i] && stash[i] != '\n')
+		i++;
+	if (!stash[i])
+		return (free(stash), NULL);
+	new = malloc(ft_strlen_gnl(stash) - i);
+	if (!new)
+		return (free(stash), NULL);
+	i++;
+	j = 0;
+	while (stash[i])
+		new[j++] = stash[i++];
+	new[j] = '\0';
+	free(stash);
+	return (new);
 }
