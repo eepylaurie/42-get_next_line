be7c5d89dbb635b57cded5bf9365e03575413cbb
eepylaurie <eepylaurie@gmail.com>
2025-12-05 22:01:05 +0100

    Fix: remove commented-out ft_strjoin_gnl function to clean up code

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: lmatthes <lmatthes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/01 22:36:30 by lmatthes          #+#    #+#             */
-/*   Updated: 2025/12/05 19:01:35 by lmatthes         ###   ########.fr       */
+/*   Updated: 2025/12/05 22:00:39 by lmatthes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,35 +38,6 @@ char	*ft_strchr_gnl(const char *s, int c)
 		return ((char *)s);
 	return (NULL);
 }
-
-// char	*ft_strjoin_gnl(char *s1, char *s2)
-// {
-// 	char	*new;
-// 	size_t	i;
-// 	size_t	j;
-
-// 	if (!s1)
-// 	{
-// 		s1 = malloc(1);
-// 		if (!s1)
-// 			return (NULL);
-// 		s1[0] = '\0';
-// 	}
-// 	new = malloc(ft_strlen_gnl(s1) + ft_strlen_gnl(s2) + 1);
-// 	if (!new)
-// 		return (free(s1), NULL);
-// 	i = 0;
-// 	while (s1[i])
-// 	{
-// 		new[i] = s1[i];
-// 		i++;
-// 	}
-// 	j = 0;
-// 	while (s2[j])
-// 		new[i++] = s2[j++];
-// 	new[i] = '\0';
-// 	return (free(s1), new);
-// }
 
 static char	*ft_strjoin_core(char *s1, char *s2, char *new)
 {
@@ -109,4 +80,3 @@ char	*ft_strjoin_gnl(char *s1, char *s2)
 	free(s1);
 	return (new);
 }
-
